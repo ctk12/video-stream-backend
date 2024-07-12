9e542f047bb6d0b8bb54b2a0ba14fb4eb8157791
ctk12 <chetankatwa12@gmail.com>
2024-07-12 13:52:54 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*', // Matches all API routes
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // For development or allowing all origins (not recommended for production)
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
